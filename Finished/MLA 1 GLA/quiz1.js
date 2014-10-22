4d8a23c864d9c350d49a7c5e8fe0b31b60cb4236
Elec0 <elec08215@gmail.com>
2014-10-22 16:16:31 -0700

    MLA 1 Finished (Finally)

--- Finished/MLA 1 GLA/quiz1.js
+++ Finished/MLA 1 GLA/quiz1.js
@@ -4,8 +4,8 @@ function loadPage()
 	curPage = "quiz1.html";
 	curQuiz=1;
 	curQuizRedirect=1;
-	addQuestion("What does MLA stand for?", "Modern literature association", "Mobile language agency", "Multicultural literature agency", "Modern language association", 4);
-	addQuestion("Where can you find information to help use MLA formatting?", "The learning center (TLC)", "College of the canyons library", "A handbook like rules for writers", "All of the above", 4);
+	addQuestion("What does MLA stand for?", "Modern Literature Association", "Mobile Language Agency", "Multicultural Literature Agency", "Modern Language Association", 4);
+	addQuestion("Where can you find information to help use MLA formatting?", "The Learning Center (TLC)", "College of the Canyons Library", "A handbook like Rules for Writers", "All of the above", 4);
 	addQuestion("When formatting a document in MLA style, how wide should your margins be?", "One inch on all four sides", "1 1/4 inches on all four sides", "Whatever my computer is set to ", "One inch on the top and bottom of the page; one and a half inches on the left and right sides", 1);
 	addQuestion("What information should appear in your header?", "Your full name", "Your last name only", "The page number only", "Your last name and the page number", 4);
 	addQuestion("Which of the following is the correct way to format a title?", "TITLE", "Title", "<b>Title</b>", "<u>Title</u>", 2);
