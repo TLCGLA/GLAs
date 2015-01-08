b34ca2f6188fcc53458a865ce32b7419b60b112a
Elec0 <elec08215@gmail.com>
2015-01-07 16:56:56 -0800

    Updated & Moved
    Updated Analysing quotes to the current version of the GLA template and
    added quiz redirects. Somehow it was a really old version.
    Moved Comma Splices to finished folder.

--- Need to be tested/Analysing Literary Quotes GLA/quiz2.js
+++ Need to be tested/Analysing Literary Quotes GLA/quiz2.js
@@ -3,6 +3,7 @@ function loadPage()
 	// addQuestion(Question, Answer1, Answer2, Answer3, Answer4, CorrectAnswer [not 0-indexed])
 	curPage = "quiz2.html";
 	curQuiz = 2;
+	curQuizRedirect=10;
 	
 	addQuestion("What is the definition of 'Summary'?", "A reader's initial emotional reaction to a quote.", "An examination of the function of a quote.", "Restating a quote in your own words to explain what it says.", "A close reading of a quote to determine what it means.", 3);
 	addQuestion("What is the definition of 'Response'?", "A reader's initial emotional reaction to a quote.", "An examination of the function of a quote.", "Restating a quote in your own words to explain what it says.", "A close reading of a quote to determine what it means.", 1);
