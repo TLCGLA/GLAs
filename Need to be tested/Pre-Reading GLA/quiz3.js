bc15564fbf6741b727544f520471cb15448975b1
ImranPoswal <impo47@gmail.com>
2015-02-20 10:51:14 -0800

    Pre-Reading and Cultural Expectations 2
    2/20 fixes

--- Need to be tested/Pre-Reading GLA/quiz3.js
+++ Need to be tested/Pre-Reading GLA/quiz3.js
@@ -10,7 +10,7 @@ function loadPage()
 	addQuestion("Pre-reading questions require students to think about what they already know about a topic.", "T", "F", "", "", 1);
 	addQuestion("Pre-reading questions help students learn the definition for difficult vocabulary words.", "T", "F", "", "", 2);
 	addQuestion("Students should avoid making predictions about a text because it will confuse them.", "T", "F", "", "", 2);
-	addQuestion("Transition words like however and for example help students make predictions.", "T", "F", "", "", 1);
+	addQuestion("Transition words like \"however,\" and \"for example,\" help students make predictions.", "T", "F", "", "", 1);
 	
 	//----------------------------------------------------------
 	display();
