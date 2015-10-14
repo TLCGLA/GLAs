7b00476ced3d383ed3fd401eff1e4cc083e86fbf
Elec0 <elec08215@gmail.com>
2015-10-14 02:04:11 -0700

    Cleaning

--- Misc/Jose/glas/js/slider/mobily-slider_files/_vti_cnf/plugins.js
+++
@@ -1,6 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|14 Feb 2012 13:36:22 -0000
-vti_extenderversion:SR|6.0.2.8161
-vti_cacheddtm:TX|14 Feb 2012 13:36:22 -0000
-vti_filesize:IR|106706
-vti_backlinkinfo:VX|js/slider/mobily-slider.html
