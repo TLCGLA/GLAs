209c617f6b80705723e90aa45e50340ad348655d
Jason Berry <bloodlustr@gmail.com>
2017-11-27 13:28:58 -0800

    Reset repo

--- Misc/Jose/glas/js/_vti_cnf/infinite-rotator.js
+++
@@ -1,4 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|04 Mar 2012 14:58:40 -0000
-vti_extenderversion:SR|6.0.2.8161
-vti_backlinkinfo:VX|run_on_sentences_x/index.htm note_taking_x/index.htm starting_an_essay_x/index.htm evaluating_online_sources_x/index.htm apa2/index.htm apa1/index.htm outlining_your_essay_x/index.htm stress_management_x/index.htm vocabulary_building_x/index.htm proofreading_x/index.htm timed_writing_x/index.htm apostrophes_x/index.htm commas_x/index.htm outlining_readings_x/index.htm drawing_inferences_x/index.htm reading_textbooks_x/index.htm remembering_what_you_read_x/index.htm math_anxiety/index.htm writers_block_x/index.htm online_research_x/index.htm thesis_development_x/index.htm mla1_x/index.htm mla2/index.htm plagiarism_x/index.htm preparing_for_finals_x/index.htm test_taking_x/index.htm sentence_fragments_x/index.htm time_management_x/index.htm
